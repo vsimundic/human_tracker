ce7ce6c88782a2adb8fc9f756b011504d8269e1d
Valentin Simundic <simundicv@gmail.com>
2020-05-20 09:07:06 +0200

    Catkinized packages and ready for using with .bag files (probably won't work as intended)

---
+++ src/HaarSvm/src/haarsvm_node.cpp
@@ -0,0 +1,351 @@
+/*
+	Software License Agreement (BSD License)
+	
+	Copyright (c) 2013, Southwest Research Institute
+	All rights reserved.
+	
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+	
+	* Redistributions of source code must retain the above copyright
+	notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright
+	notice, this list of conditions and the following disclaimer in the
+	documentation and/or other materials provided with the distribution.
+	* Neither the name of the Southwest Research Institute, nor the names
+	of its contributors may be used to endorse or promote products derived
+	from this software without specific prior written permission.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "HaarSvm/haarsvm.hpp"
+
+#include "ros/ros.h"
+#include <sstream>
+
+//Publish Messages
+#include "roi_msgs/RoiRect.h"
+#include "roi_msgs/Rois.h"
+#include "std_msgs/String.h"
+
+//Time Synchronizer
+// NOTE: Time Synchronizer conflicts with QT includes may need to investigate
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+//Subscribe Messages
+#include <sensor_msgs/Image.h>
+#include <stereo_msgs/DisparityImage.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
+#include </home/valentin/cv_bridge/include/cv_bridge/CvBridge.h>
+
+
+// Image Transport
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+
+// Used to display OPENCV images
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace stereo_msgs;
+using namespace message_filters::sync_policies;
+using namespace roi_msgs;
+using namespace sensor_msgs;
+using namespace sensor_msgs::image_encodings;
+using sensor_msgs::Image;
+using cv_bridge::CvImagePtr;
+using std::vector;
+using std::string;
+using cv::Rect;
+using cv::Mat;
+
+/*
+	using std::vector;
+	using std::string;
+	using cv::Rect;
+	using cv::Mat;
+*/
+class HaarSvmNode
+{
+	private:
+	// Define Node
+	ros::NodeHandle node_;
+	
+	// Subscribe to Messages
+	message_filters::Subscriber<Image> sub_disparity_;
+	message_filters::Subscriber<Image> sub_image_;
+	message_filters::Subscriber<Rois> sub_rois_;
+	
+	// Define the Synchronizer
+	typedef ApproximateTime<Image, Image, Rois> ApproximatePolicy;
+	typedef message_filters::Synchronizer<ApproximatePolicy> ApproximateSync;
+	boost::shared_ptr<ApproximateSync> approximate_sync_;
+	
+	// Messages to Publish
+	ros::Publisher pub_rois_;
+	ros::Publisher pub_Color_Image_;
+	ros::Publisher pub_Disparity_Image_;
+	
+	Rois output_rois_;
+	
+	enum {ACCUMULATE=0, TRAIN, DETECT, EVALUATE, LOAD};
+	
+	//Define the Classifier Object
+	HaarSvm::HaarSvmClassifier HSC_;
+	int num_class1;
+	int num_class0;
+	int num_TP_class1;
+	int num_FP_class1;
+	int num_TP_class0;
+	int num_FP_class0;
+	
+	public:
+	
+	explicit HaarSvmNode(const ros::NodeHandle& nh):
+    node_(nh)
+	{
+		num_class1 = 0;
+		num_class0 = 0;
+		num_TP_class1 = 0;
+		num_FP_class1 = 0;
+		num_TP_class0 = 0;
+		num_FP_class0 = 0;
+		
+		string nn = ros::this_node::getName();
+		int qs;
+		if(!node_.getParam(nn + "/Q_Size",qs)){
+			qs=3;
+		}
+		
+		int NS;
+		if(!node_.getParam(nn + "/num_Training_Samples",NS)){
+			NS = 350;
+			node_.setParam(nn + "/num_Training_Samples",NS);
+		}
+		HSC_.setMaxSamples(NS);
+		
+		// Published Messages
+		pub_rois_= node_.advertise<Rois>("HaarSvmOutputRois",qs);
+		pub_Color_Image_ = node_.advertise<Image>("HaarSvmColorImage",qs);
+		pub_Disparity_Image_= node_.advertise<Image>("HaarSvmDisparityImage",qs);
+		
+		
+		// Subscribe to Messages
+		sub_image_.subscribe(node_,"Color_Image",qs);
+		sub_disparity_.subscribe(node_, "Disparity_Image",qs);
+		sub_rois_.subscribe(node_,"input_rois",qs);
+		
+		// Sync the Synchronizer
+		approximate_sync_.reset(new ApproximateSync(ApproximatePolicy(qs), 
+			sub_image_, 
+			sub_disparity_,
+		sub_rois_));
+		
+		approximate_sync_->registerCallback(boost::bind(&HaarSvmNode::imageCb,
+			this,
+			_1,
+			_2,
+		_3));
+	}
+	int get_mode()
+	{
+		
+		int callback_mode;
+		std::string mode="";
+		node_.param(ros::this_node::getName() + "/mode", mode, std::string("none"));
+		if(mode.compare("detect") == 0)
+		{
+			callback_mode = DETECT;
+		}
+		else if(mode.compare("train")==0)
+		{
+			callback_mode = TRAIN;
+		}
+		else if(mode.compare("load")==0)
+		{
+			callback_mode = LOAD;
+		}
+		else if(mode.compare("evaluate")==0)
+		{
+			callback_mode = EVALUATE;
+		}
+		else if(mode.compare("accumulate")==0)
+		{
+			callback_mode = ACCUMULATE;
+		}
+		else // default mode is accumulate
+		{
+			callback_mode = ACCUMULATE;
+		}
+		return(callback_mode);
+	}
+	void imageCb(const ImageConstPtr& image_msg,
+		const ImageConstPtr& disparity_msg,
+		const RoisConstPtr& rois_msg){
+		
+		bool label_all;
+		vector<Rect> R_out;
+		vector<int> L_out;
+		string param_name;
+		string nn = ros::this_node::getName();
+		string cfnm;
+		int numSamples;
+		
+		//Use CV Bridge to convert image
+		sensor_msgs::CvBridge bridge;
+		IplImage* ipl_im = bridge.imgMsgToCv(image_msg, image_encodings::MONO8);
+		Mat im_gray  = Mat(ipl_im);
+		// CvImagePtr cv_gray  = cv_bridge::toCvCopy(image_msg,image_encodings::MONO8);
+		// Mat im_gray  = cv_gray->image;		
+		
+		// take the region of interest message and create vectors of ROIs and labels
+		vector<int> L_in;
+		vector<Rect> R_in;
+		R_in.clear();
+		L_in.clear();
+		for(unsigned int i=0;i<rois_msg->rois.size();i++){
+			int x = rois_msg->rois[i].x;
+			int y = rois_msg->rois[i].y;
+			int w = rois_msg->rois[i].width;
+			int h = rois_msg->rois[i].height;
+			int l = rois_msg->rois[i].label;
+			Rect R(x,y,w,h);
+			R_in.push_back(R);
+			L_in.push_back(l);
+		}
+		
+		// get parameter for controlling point along ROC 
+		param_name = nn + "/HaarSvmThreshold";
+		double temp=0.0;
+		node_.getParam(param_name,temp);
+		HSC_.HaarSvmThreshold_ = (float)temp;
+		//    ROS_ERROR("HaarSvm Threshold = %f",HSC_.HaarSvmThreshold_);
+		
+		// do the work of the node
+		switch(get_mode()){
+			case DETECT:
+			label_all = false;
+			HSC_.detect(R_in,L_in,im_gray,R_out,L_out,label_all);
+			output_rois_.rois.clear();
+			output_rois_.header.stamp = image_msg->header.stamp;
+			output_rois_.header.frame_id = image_msg->header.frame_id;
+			ROS_INFO("HaarSvm found %d objects",(int)L_out.size());
+			for(unsigned int i=0; i<R_out.size();i++){
+				RoiRect R;
+				R.x      = R_out[i].x;
+				R.y      = R_out[i].y;
+				R.width  = R_out[i].width;
+				R.height = R_out[i].height;
+				R.label  = L_out[i];
+				output_rois_.rois.push_back(R);
+			}
+			pub_rois_.publish(output_rois_);
+			pub_Color_Image_.publish(image_msg);
+			pub_Disparity_Image_.publish(disparity_msg);
+			break;
+			case ACCUMULATE:
+			numSamples = HSC_.addToTraining(R_in,L_in,im_gray);
+			param_name = nn + "/num_Training_Samples";
+			int NS;
+			if(node_.getParam(param_name,NS)){
+				float percent = (float)HSC_.numSamples_ * 100.0/NS;
+				ROS_INFO("ACCUMULATING: %6.1f%c",percent,'%');
+				if(numSamples >= NS){
+					param_name = nn + "/mode";
+					node_.setParam(param_name, std::string("train"));
+					ROS_ERROR("DONE Accumulating, switching to train mode");
+				}
+			}
+			break;
+			case TRAIN:
+			param_name = nn + "/classifier_file";
+			node_.param(param_name,cfnm,std::string("/home/clewis/classifiers/test.xml"));
+			ROS_ERROR("Submitting %d Samples to Train ouput= %s",HSC_.numSamples_,cfnm.c_str());
+			HSC_.train(cfnm);
+			param_name = nn + "/mode";
+			node_.setParam(param_name, std::string("evaluate"));
+			ROS_ERROR("DONE TRAINING, switching to evaluate mode");
+			break;
+			case EVALUATE:
+			{
+				if(!HSC_.loaded){
+					param_name = nn + "/classifier_file";
+					node_.param(param_name,cfnm,std::string("test.xml"));
+					ROS_ERROR("HaarSvm LOADING  %s",cfnm.c_str());
+					HSC_.load(cfnm);
+				}
+				label_all = false;
+				HSC_.detect(R_in,L_in,im_gray,R_out,L_out,label_all);
+				
+				int total0_in_list=0;
+				int total1_in_list=0;
+				int fp_in_list=0;
+				int tp_in_list=0;
+				
+				// count the input labels
+				for(unsigned int i=0; i<R_in.size();i++){
+					if(L_in[i] == 0 || L_in[i] == -1) total0_in_list++;
+					if(L_in[i] == 1) total1_in_list++;
+				}
+				num_class0 += total0_in_list;
+				num_class1 += total1_in_list;
+				
+				// count the output labels which have the correct and incorrect label
+				for(unsigned int i=0; i<R_out.size();i++){
+					if(L_out[i] == 1){
+						tp_in_list++;
+					}
+					else fp_in_list++;
+				}
+				num_TP_class1 += tp_in_list;
+				num_FP_class1 += fp_in_list;
+				num_TP_class0 += total0_in_list - fp_in_list;
+				num_FP_class0 += total1_in_list - tp_in_list;
+				float tp1 = (float)num_TP_class1 / (float) num_class1 * 100.0;
+				float tp0 = (float)num_TP_class0 / (float) num_class0 * 100.0;
+				float fp1 = (float)num_FP_class1 / (float) num_class1 * 100.0;
+				float fp0 = (float)num_FP_class0 / (float) num_class0 * 100.0;
+				ROS_ERROR("TP0 = %6.2f FP0 =  %6.2f TP1 = %6.2f FP1 =  %6.2f",tp0,fp0,tp1,fp1);
+			}
+			break;
+			case LOAD:
+			param_name = nn + "/classifier_file";
+			node_.param(param_name,cfnm,std::string("test.xml"));
+			ROS_ERROR("HaarSvm LOADING  %s",cfnm.c_str());
+			HSC_.load(cfnm);
+			param_name = nn + "/mode";
+			node_.setParam(param_name, std::string("detect"));
+			break;
+		}// end switch
+		
+	}
+	~HaarSvmNode()
+	{
+	}
+};
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "HaarSvm");
+	ros::NodeHandle n;
+	HaarSvmNode HN(n);
+	ros::spin();
+	return 0;
+}
+
---
+++ src/consistency/src/consistency_node.cpp
@@ -0,0 +1,399 @@
+/*
+	Software License Agreement (BSD License)
+	
+	Copyright (c) 2013, Southwest Research Institute
+	All rights reserved.
+	
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+	
+	* Redistributions of source code must retain the above copyright
+	notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright
+	notice, this list of conditions and the following disclaimer in the
+	documentation and/or other materials provided with the distribution.
+	* Neither the name of the Southwest Research Institute, nor the names
+	of its contributors may be used to endorse or promote products derived
+	from this software without specific prior written permission.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+#include <string>
+#include <vector>
+#include <sstream>
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+// Ros package includes
+#include <consistency/consistency.hpp>
+#include </home/valentin/human_tracker_ws/src/roi_msgs/include/roi_msgs/overlap.hpp>
+#include <roi_msgs/RoiRect.h>
+#include <roi_msgs/Rois.h>
+
+//Time Synchronizer
+// NOTE: Time Synchronizer conflicts with QT includes may need to investigate
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+//Subscribe Messages
+#include <sensor_msgs/Image.h>
+#include <stereo_msgs/DisparityImage.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
+// #include <cv_bridge/cvBridge.h>
+// #include </home/valentin/human_tracker_ws/src/cv_bridge/include/cv_bridge/cv_bridge.h>
+#include </home/valentin/cv_bridge/include/cv_bridge/CvBridge.h>
+
+// Image Transport
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+
+// Used to display OPENCV images
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace sensor_msgs;
+using namespace stereo_msgs;
+using namespace message_filters::sync_policies;
+using namespace sensor_msgs::image_encodings;
+using namespace roi_msgs;
+using std::string;
+using Cten::Consistency;
+using namespace cv_bridge;
+
+class consistencyNode
+{
+	private:
+	// Define Node
+	ros::NodeHandle node_;
+	
+	// Subscribe to Messages
+	message_filters::Subscriber<Image> sub_disparity_;
+	message_filters::Subscriber<Image> sub_image_;
+	message_filters::Subscriber<Rois> sub_rois_;
+	
+	// Define the Synchronizer
+	typedef ApproximateTime<Image, Image, Rois> ApproximatePolicy;
+	typedef ApproximateTime<Image, Image> ApproximatePolicy2;
+	typedef message_filters::Synchronizer<ApproximatePolicy> ApproximateSync;
+	typedef message_filters::Synchronizer<ApproximatePolicy2> ApproximateSync2;
+	boost::shared_ptr<ApproximateSync> approximate_sync_;
+	boost::shared_ptr<ApproximateSync2> approximate_sync2_;
+	
+	// Messages to Publish
+	ros::Publisher pub_rois_;
+	ros::Publisher pub_Color_Image_;
+	ros::Publisher pub_Disparity_Image_;
+	
+	Rois output_rois_;
+	
+	//Define the Classifier Object
+	Consistency con_;
+	
+	//Mode of operation
+	string mode;
+	bool useDefaultRoi;
+	public:
+	
+	explicit consistencyNode(const ros::NodeHandle& nh):
+    node_(nh)
+	{
+		string nn = ros::this_node::getName();
+		int qs;
+		if(!node_.getParam(nn + "/Q_Size",qs)){
+			qs=3;
+		}
+		
+		// Published Messages
+		pub_rois_           = node_.advertise<Rois>("ConsistencyOutputRois",qs);
+		pub_Color_Image_    = node_.advertise<Image>("ConsistencyColorImage",qs);
+		pub_Disparity_Image_= node_.advertise<Image>("ConsistencyDisparityImage",qs);
+		
+		// Subscribe to Messages
+		sub_image_.subscribe(node_,"Color_Image",qs);
+		sub_disparity_.subscribe(node_, "Disparity_Image",qs);
+		// ROS_ERROR("height = %d, width = %d", sub_disparity_.height, sub_disparity_.width);
+		
+		
+		if(!node_.getParam(nn + "/UseDefaultRois",useDefaultRoi)){
+			useDefaultRoi=false;
+		}
+		if(useDefaultRoi){ // no roi available, use default
+			ROS_ERROR("Using default roi");
+			// sync the syncronizer
+			approximate_sync2_.reset(new ApproximateSync2(ApproximatePolicy2(qs), sub_image_, sub_disparity_));
+			// ROS_ERROR("Proso sam reset");
+			// register the 2 element callback
+			approximate_sync2_->registerCallback(boost::bind(&consistencyNode::imageCb2, this, _1, _2));
+			// ROS_ERROR("Prosos sam registerCallback imageCb2");
+		}
+		else{
+			// subscribe to the rois
+			sub_rois_.subscribe(node_,"input_rois",1);
+			
+			// Sync the Synchronizer
+			approximate_sync_.reset(new ApproximateSync(ApproximatePolicy(4), 
+				sub_image_, 
+				sub_disparity_,
+			sub_rois_));
+			
+			// register the 3 element callback
+			approximate_sync_->registerCallback(boost::bind(&consistencyNode::imageCb,
+				this,
+				_1,
+				_2,
+			_3));
+		}
+		
+		// load the classifier configuration
+		string dfn= nn + "/yml/constraints";//default filename
+		node_.param(nn + "/yaml_filename",con_.ymlFilename,dfn);
+		con_.load();
+		
+		// The label of interest is:
+		node_.param(ros::this_node::getName()+"/label",con_.label,1);
+		
+		// mode to start with
+		mode="";
+		node_.param(ros::this_node::getName() + "/mode", mode, string("none"));
+		ROS_INFO("Selected mode: %s",mode.c_str());
+		con_.mode = mode;
+		
+		// number of data to train with
+		int dm=1000;
+		node_.param(ros::this_node::getName() + "/max_training_samples", con_.max_training_data, dm);
+		
+		if(mode.compare("detect") == 0){
+			ROS_INFO("Starting in detection mode");
+		}
+		else if(mode.compare("train")==0){
+			ROS_ERROR("Starting in training mode is non-sensical");
+		}
+		else if(mode.compare("load")==0){ //mode is accumulate
+			ROS_INFO("Starting by loading consistency data");
+		}
+		else if(mode.compare("accumulate")==0){
+			ROS_INFO("Starting accumulation of training data");
+		}
+		else{
+			ROS_ERROR("INVALID STARTING MODE: switching to accumulate");
+			con_.mode = "accumulate";
+			node_.setParam(ros::this_node::getName() + "/mode", con_.mode);
+		}
+		
+	}
+	void imageCb2(const ImageConstPtr& image_msg, const ImageConstPtr& disparity_msg)
+	{
+		// ROS_ERROR("Uso sam u imageCb2");
+		RoisPtr P(new Rois);
+		
+		P->rois.clear();
+		P->header.stamp    = image_msg->header.stamp;
+		P->header.frame_id = image_msg->header.frame_id;
+		
+		// cv::imshow("view", cv_bridge::toCvShare(disparity_msg, "bgr8")->image);
+		// cv::waitKey(0);
+		// ROS_ERROR("I sta cemo sad kad smo tu dosli?");
+		RoiRect dR;
+		dR.x = 0;
+		dR.y = 0;
+		dR.height = image_msg->height;
+		dR.width  = image_msg->width;
+		dR.label  = 0;
+		P->rois.push_back(dR);  
+		// ROS_ERROR("Pozvat cu imageCb");
+		imageCb(image_msg,disparity_msg,P);
+	}
+	
+	void imageCb(const ImageConstPtr& image_msg, const ImageConstPtr& disparity_msg, const RoisConstPtr& rois_msg)
+	{
+		// ROS_ERROR("Uso sam u imageCb");
+		
+		string nn = ros::this_node::getName();
+		string mode="";
+		node_.param(nn+ "/mode", mode, string("none"));
+		con_.mode = mode;
+		bool kinect_disparity_fix;
+		node_.param(nn+ "Kinect_Disparity_Fix",kinect_disparity_fix,false);
+		// ROS_ERROR("Ovdje sam negdje i ne znam sta je ovo");
+		float percent_done=0.0;
+		//Use CV Bridge to convert images	
+		// cv_bridge::CvImagePtr cv_color = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
+		// ROS_ERROR("Ispred ovog cvImagePtr toCvCopyja sam!");
+		
+		// ROS_ERROR("Could not convert from '%s' to 'bgr8'.", typeid(disparity_msg).name());
+		CvImagePtr cv_color;
+		
+		// ROS_ERROR("DJIHA!");
+		// ROS_ERROR("cv_bridge exception: %s", e.what());
+		// ROS_ERROR("PRRRRRRA!");
+		// ROS_ERROR("%s", image_msg->encoding.c_str());
+		// Image img;
+		// img.header = image_msg->header;
+		// img.height = image_msg->height;
+		// img.width = image_msg->width;
+		// img.is_bigendian = image_msg->is_bigendian;
+		// img.step = image_msg->step;
+		// img.data = image_msg->data;
+		// img.encoding = sensor_msgs::image_encodings::BGR8;
+		
+		// ROS_ERROR("ENCODING JE ZAJEBANOO!");
+		// cv_color = toCvCopy(img);
+		sensor_msgs::CvBridge bridge;
+		
+		IplImage* ipl_im = bridge.imgMsgToCv(image_msg, "bgr8");
+		
+		// ROS_ERROR("ENCODING NIJE ZAJEBANOO!");
+		
+		
+		
+		// ROS_ERROR("Aaaa");
+		// ROS_ERROR("Disparity image encoding = '%s'", disparity_msg->encoding.c_str());
+		// ROS_ERROR(disparity_msg->encoding == sensor_msgs::image_encodings::TYPE_32FC1);
+		assert(disparity_msg->encoding == sensor_msgs::image_encodings::TYPE_32FC1);
+		// ROS_ERROR("BALABLBALABALBALBALAB.");
+		cv::Mat_<float> dmat(disparity_msg->height, disparity_msg->width, (float*) &disparity_msg->data[0], disparity_msg->step);
+		
+		// **********************************************************************//
+		// between these comments lies a hack that accounts for the difference   //
+		// between the focal lengths of the kinect's color camera and ir cameras //
+		// TODO, account for this using proper calibration and projection        //
+		if(kinect_disparity_fix){
+			int nrows = 434;
+			int ncols = 579;
+			int row_offset = (dmat.rows - nrows)/2;
+			int col_offset = (dmat.cols - ncols)/2;
+			cv::Mat Scaled_Disparity(nrows,ncols,CV_32FC1);
+			resize(dmat,Scaled_Disparity,cv::Size(ncols,nrows),0,0, CV_INTER_NN );
+			for(int i=0;i<dmat.rows;i++){
+				float *rp = &dmat.at<float>(i,0);
+				for(int j=0;j<dmat.cols;j++){
+					*rp = 0.0;
+					rp++;
+				}
+			}
+			for(int i=0;i<nrows;i++){
+				float *rp = &dmat.at<float>(i+row_offset,col_offset);
+				float *rp2 = &Scaled_Disparity.at<float>(i,0);
+				for(int j=0;j<ncols;j++){
+					*rp = *rp2;
+					rp++;
+					rp2++;
+				}
+			}
+		}
+		// **********************************************************************//
+		
+		//load ROS rois.msg data into vector of CvRect and labels for Classifier to use
+		RoiRect roi;
+		CvRect cvRoi;
+		con_.cvRoisInput.clear();
+		con_.labelsInput.clear();
+		for(unsigned int i = 0;i<rois_msg->rois.size();i++){
+			roi = rois_msg->rois[i];
+			cvRoi.x = roi.x;
+			cvRoi.y = roi.y;
+			cvRoi.width = roi.width;
+			cvRoi.height = roi.height;			
+			con_.cvRoisInput.push_back(cvRoi);
+			con_.labelsInput.push_back(roi.label);	
+		}
+		
+		// pass Color and Disparity Image to consistency object
+		con_.color_image=cv::Mat(ipl_im);
+		con_.disparity_image=dmat;
+		// delete ipl_im;
+		// ipl_im = NULL;
+		// do the work of the callback
+		if(con_.mode.compare("detect") == 0){
+			con_.detect();
+		}
+		else if(con_.mode.compare("train")==0){
+			ROS_ERROR("starting training");
+			con_.train();
+			ROS_ERROR("done training switch to detect");
+			con_.mode = "detect";
+			node_.setParam(ros::this_node::getName() + "/mode", con_.mode);
+		}
+		else if(con_.mode.compare("accumulate")==0){
+			con_.accumulate();
+			percent_done = (float) con_.TD_.size()/(float)con_.max_training_data  * 100.0;
+			if((int)con_.TD_.size()>= con_.max_training_data){
+				con_.mode = "train";
+				node_.setParam(ros::this_node::getName() + "/mode", con_.mode);
+			}
+		}
+		else if(mode.compare("load")==0){
+			ROS_ERROR("consistency LOADING %s",con_.ymlFilename.c_str());
+			con_.load();
+			con_.mode = "detect";
+			node_.setParam(ros::this_node::getName() + "/mode", con_.mode);
+		}
+		else{
+			ROS_ERROR("INVALID MODE: switching to accumulate");
+			con_.mode = "accumulate";
+			node_.setParam(ros::this_node::getName() + "/mode", con_.mode);
+		}
+		
+		//transfer results to output rois.msg
+		output_rois_.rois.clear();
+		output_rois_.header.stamp = image_msg->header.stamp;
+		output_rois_.header.frame_id = image_msg->header.frame_id;
+		
+		for(unsigned int j=0;j<con_.cvRoisOutput.size();j++){
+			roi.x = con_.cvRoisOutput[j].x;
+			roi.y = con_.cvRoisOutput[j].y;
+			roi.width = con_.cvRoisOutput[j].width;
+			roi.height = con_.cvRoisOutput[j].height;
+			roi.label = con_.labelsOutput[j];
+			output_rois_.rois.push_back(roi);
+		}
+		
+		/*
+			remove_overlap_Rois(output_rois_, non_overlapping_rois_);
+			non_overlapping_rois_.header.stamp    = image_msg->header.stamp;
+			non_overlapping_rois_.header.frame_id = image_msg->header.frame_id;
+			int s1 = output_rois_.rois.size();
+			int s2 = non_overlapping_rois_.rois.size();
+			ROS_ERROR("output_rois_.size = %d non_overlapping size = %d",s1,s2);
+		*/
+		// publish results
+		if(mode.compare("accumulate")==0){
+			ROS_INFO("ACCUMULATING TRAINING DATA: %5.1f%c done",percent_done,'%');
+		}
+		else{
+			ROS_INFO("PUBLISHED: %d Consistency ROIs", int(output_rois_.rois.size()));
+			pub_rois_.publish(output_rois_);
+			pub_Color_Image_.publish(image_msg);
+			pub_Disparity_Image_.publish(disparity_msg);
+		}
+		// ROS_ERROR("ZAVRSIO SAM OVO SVE TU.");
+	}
+	~consistencyNode()
+	{
+	}
+};
+
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "consistency");
+	ros::NodeHandle n;
+	consistencyNode CN(n);
+	ros::spin();
+	return 0;
+}
+
